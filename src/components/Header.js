ce2a2dc0460b63df286e9b19c62d1553e51ff97d
Aayushi Iyer <51299602+moonshot-aayushi@users.noreply.github.com>
2020-04-04 17:59:43 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import Container from 'components/Container';
+import Container from './components/Container';
 
 const Header = () => {
   return (
