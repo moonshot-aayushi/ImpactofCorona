bf36d6df54a23a9d623318f34304627e9ec8e51b
Aayushi Iyer <51299602+moonshot-aayushi@users.noreply.github.com>
2020-04-04 18:42:40 +0530

    Update useConfigureLeaflet.js

--- src/hooks/useConfigureLeaflet.js
+++ src/hooks/useConfigureLeaflet.js
@@ -1,6 +1,6 @@
 import L from 'leaflet';
 
-import { isDomAvailable } from 'lib/util';
+import { isDomAvailable } from './lib/util';
 
 const useConfigureLeaflet = () => {
   if ( !isDomAvailable()) return;
