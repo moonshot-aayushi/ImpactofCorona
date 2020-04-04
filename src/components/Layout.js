dac75ecce34c5039d0daacdcee9446d4878c7335
Aayushi Iyer <51299602+moonshot-aayushi@users.noreply.github.com>
2020-04-04 18:00:19 +0530

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
-import 'assets/stylesheets/application.scss';
+import './assets/stylesheets/application.scss';
 
-import Header from 'components/Header';
-import Footer from 'components/Footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const Layout = ({ children, pageName }) => {
   let className = '';
