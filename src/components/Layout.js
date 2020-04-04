37cde40ae8e7338ae39aa5d26cfeef6415a6d7d4
Aayushi Iyer <51299602+moonshot-aayushi@users.noreply.github.com>
2020-04-04 18:10:19 +0530

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
-import './assets/stylesheets/application.scss';
+import './src/assets/stylesheets/application.scss';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
