c306d49868b646569950c152acef1ef9df724869
Aayushi Iyer <51299602+moonshot-aayushi@users.noreply.github.com>
2020-04-04 14:50:41 +0530

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 
-import Layout from 'components/Layout';
-import Container from 'components/Container';
+import Layout from 'components./Layout';
+import Container from 'components./Container';
 
 const SecondPage = () => {
   return (
