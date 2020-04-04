4af48ae520c0a271e8e97037ca76d5d3d396785a
Aayushi Iyer <51299602+moonshot-aayushi@users.noreply.github.com>
2020-04-04 17:06:17 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,9 @@ import L from 'leaflet';
 
 
 
-import Layout from 'components/Layout';
-import Container from 'components/Container';
-import Map from 'components/Map';
+import Layout from './components/Layout';
+import Container from './components/Container';
+import Map from './components/Map';
 
 // import gatsby_astronaut from 'assets/images/gatsby-astronaut.jpg';
 
